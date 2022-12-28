d0c445a784aa93408efaf0586b641d155ad0640a
Francesco Piccoli <bruce.banner.2016@hotmail.com>
2022-12-28 02:09:05 +0100

    riaggiunta moltiplicazione

--- function.php
+++ function.php
@@ -2,16 +2,22 @@
 	
 	/* addizione */
 
-	function addizione (n1, n2) { retunr n1 + n2 }
+	function addizione (n1, n2) { retunr n1 + n2; }
 
 	/* sottrazione */
 
-	function sottrazione (n1, n2) { retunr n1 - n2 }
+	function sottrazione (n1, n2) { retunr n1 - n2; }
 	
 	/* divisione */
 	
 	function divisione (n1, n2) {
-		if( n2 != 0) { return n1 / n2}
+		if( n2 != 0) { return n1 / n2;}
 		else { return "errore";}
+	}
+	
+	/* moltiplicazione */
+	
+	function moltiplicazione (n1, n2) {
+		return n1 * n2;
 	}
 ?>
